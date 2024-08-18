ed23f56c5339d28a2538b1c1970fbbdcec68b879
Streltsov Ivan <principdereva@gmail.com>
2024-08-18 14:09:42 +0200

    feat: fonts

--- landing/next-app/src/pages/_app.tsx
+++ landing/next-app/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/index.scss';
 import type { AppProps } from 'next/app';
+import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
